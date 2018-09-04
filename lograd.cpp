5c2035b1c7b30b0fe6a958a1d359de0705fc678a
Clayton Bauman <claytonkb@gmail.com>
2018-09-04 09:49:15 -0700

    Misc changes

--- lograd.cpp
+++ lograd.cpp
@@ -48,6 +48,8 @@ void dev_prompt(void){
 
     _say("type 0 for menu");
     _msg("XXX: add not_cell");
+    _msg("XXX: AD algo to compile down entire circuit?");
+    _msg("XXX: finite-diff approximation to sanity-check the gradient");
 
 //    ifstream t("test.lgc");
     bool lgc_file_processed;
